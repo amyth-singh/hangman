a4a0c292dc8adb01a1258e0a81bdf43a45f33c4c
amyth-singh <theamithsingh@gmail.com>
2023-01-31 23:17:00 +0000

    variable CONDITION was created and set to 0

--- milestone_3.py
+++ milestone_3.py
@@ -3,6 +3,8 @@ import random           # Import 'random' module from Python
 word_list = ["apple", "kiwi", "orange", "guava", "plum"] # Variable with list of items to randamise from
 word = (random.choice(word_list)) # 'choice' method applied on variable to randomise
 
+condition = 0
+
 while(condition == True):
     guess = input("Guess a letter: ")
     if (guess.isalpha() == True) and (len(guess) == 1):
