e0348465ece2730cfa07d9face329be62b01ec0b
amyth-singh <theamithsingh@gmail.com>
2023-01-31 23:11:19 +0000

    Added an IF LOOP to the statment

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,16 @@
+import random           # Import 'random' module from Python
+
+word_list = ["apple", "kiwi", "orange", "guava", "plum"] # Variable with list of items to randamise from
+word = (random.choice(word_list)) # 'choice' method applied on variable to randomise
+
 while(True):
     guess = input("Guess a letter: ")
     if (guess.isalpha() == True) and (len(guess) == 1):
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word")
+else:
+    print(f"Sorry, {guess} is not in the word")
