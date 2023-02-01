7c6a0095da77ae81090f24564b10705cd8c1e5f1
amyth-singh <theamithsingh@gmail.com>
2023-02-01 16:21:40 +0000

    defined 2 functions, check_guess and ask_for_input

--- milestone_3.py
+++ milestone_3.py
@@ -3,16 +3,21 @@ import random           # Import 'random' module from Python
 word_list = ["apple", "kiwi", "orange", "guava", "plum"] # Variable with list of items to randamise from
 word = (random.choice(word_list)) # 'choice' method applied on variable to randomise
 
-condition = 0
+def check_guess(guess):
+  guess.lower()
+  if guess in word:
+    print(f"Good guess! {guess} is in the word")
+  else:
+    print(f"Sorry, {guess} is not in the word")
 
-while(condition == True):
-    guess = input("Guess a letter: ")
+def ask_for_input():
+  guess = input("Guess a letter: ")
+  check_guess(guess)
+  while(True):
     if (guess.isalpha() == True) and (len(guess) == 1):
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+    return ask_for_input()
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word")
-else:
-    print(f"Sorry, {guess} is not in the word")
+ask_for_input()
