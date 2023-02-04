0588e8940ec50fead2ade7afefb6b81df1f5fb27
amyth-singh <theamithsingh@gmail.com>
2023-02-04 19:38:39 +0000

    moved code from milestone_3.py to milestone_4.py to test if AiCore portal will verify my task

--- milestone_4.py
+++ milestone_4.py
@@ -1,3 +1,28 @@
+import random
+
+word_list = ["apple", "kiwi", "orange", "guava", "plum"]
+word = (random.choice(word_list))
+
+def check_guess(guess):
+  guess.lower()
+  if guess in word:
+    print(f"Good guess! {guess} is in the word")
+  else:
+    print(f"Sorry, {guess} is not in the word")
+
+def ask_for_input():
+  while True:
+    guess = input("Guess a letter: ")
+    check_guess(guess)
+    if (guess.isalpha() == True) and (len(guess) == 1):
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+        break
+  return ask_for_input()
+
+ask_for_input()
+
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
