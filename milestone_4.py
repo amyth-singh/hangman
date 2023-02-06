681edcc71ffc18c422448ad020141967df249c38
amyth-singh <theamithsingh@gmail.com>
2023-02-06 15:17:37 +0000

    CLASS HANGMAN defined with __INIT__ ATRRIBUTES

--- milestone_4.py
+++ milestone_4.py
@@ -1,32 +1,13 @@
 import random
 
-word_list = ["apple", "kiwi", "orange", "guava", "plum"]
-word = (random.choice(word_list))
-
-def check_guess(guess):
-  guess.lower()
-  if guess in word:
-    print(f"Good guess! {guess} is in the word")
-  else:
-    print(f"Sorry, {guess} is not in the word")
-
-def ask_for_input():
-  while True:
-    guess = input("Guess a letter: ")
-    check_guess(guess)
-    if (guess.isalpha() == True) and (len(guess) == 1):
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-        break
-  return ask_for_input()
-
-ask_for_input()
-
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word_guessed = word_guessed
-        self.num_letters = num_letters
-        self.list_of_guesses = list_of_guesses
+        self.word = (random.choice(word_list))
+        self.word_guessed = ['_']*len(self.word)
+        self.num_letters = len(list(set(self.word_guessed)))
+        self.list_of_guesses = []
+        print(self.word, self.word_guessed)
+
+a = Hangman(["apple", "kiwi"])
