eee0629a437706f06a8ab3911fe9ca21f316913f
amyth-singh <theamithsingh@gmail.com>
2023-02-06 15:19:34 +0000

    made change to self.num_letters

--- milestone_4.py
+++ milestone_4.py
@@ -6,7 +6,7 @@ class Hangman:
         self.num_lives = num_lives
         self.word = (random.choice(word_list))
         self.word_guessed = ['_']*len(self.word)
-        self.num_letters = len(list(set(self.word_guessed)))
+        self.num_letters = len(list(set(self.word)))
         self.list_of_guesses = []
         print(self.word, self.word_guessed)
 
