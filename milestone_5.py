50a3782f8403eb64a7295b6b193733fd54f40b98
amyth-singh <theamithsingh@gmail.com>
2023-02-08 20:31:24 +0000

    completing the game

--- milestone_5.py
+++ milestone_5.py
@@ -1,14 +1,16 @@
 from milestone_4 import Hangman
 
-def play_game(word_list):
+def play_game(word_list, num_lives=5):
   num_lives=5
   game = Hangman(word_list, num_lives)
   while True:
     if game.num_lives == 0:
       print("You lost!")
+      break
     if game.num_letters > 0:
       game.ask_for_input()
     elif game.num_lives != 0 or game.num_letters > 0:
       print("Congragulations. You won the game!")
+      break
 
 play_game(["apple", "plum"])
