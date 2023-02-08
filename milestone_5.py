375c84cfb2528db4d258877b5c7158b462297205
amyth-singh <theamithsingh@gmail.com>
2023-02-08 20:39:30 +0000

    PRINT spelling issue

--- milestone_5.py
+++ milestone_5.py
@@ -1,17 +1,16 @@
 from milestone_4 import Hangman
 
-def Hangman():
-    def play_game(word_list, num_lives=5):
-        num_lives=5
-        game = Hangman(word_list, num_lives)
-        while True:
-            if game.num_lives == 0:
-                print("You lost!")
-                break
-            if game.num_letters > 0:
-                game.ask_for_input()
-                elif game.num_lives != 0 or game.num_letters > 0:
-                    print("Congragulations. You won the game!")
-                    break
+def play_game(word_list, num_lives=5):
+    num_lives=5
+    game = Hangman(word_list, num_lives)
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        if game.num_letters > 0:
+            game.ask_for_input()
+        elif game.num_lives != 0 or game.num_letters > 0:
+            print("Congratulations. You won the game!")
+            break
 
 play_game(["apple", "plum"])
