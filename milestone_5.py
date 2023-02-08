77ae02ff0acff83832d7db01e7e6023ab73838e1
amyth-singh <theamithsingh@gmail.com>
2023-02-08 20:34:08 +0000

    bug

--- milestone_5.py
+++ milestone_5.py
@@ -1,16 +1,17 @@
 from milestone_4 import Hangman
 
-def play_game(word_list, num_lives=5):
-  num_lives=5
-  game = Hangman(word_list, num_lives)
-  while True:
-    if game.num_lives == 0:
-      print("You lost!")
-      break
-    if game.num_letters > 0:
-      game.ask_for_input()
-    elif game.num_lives != 0 or game.num_letters > 0:
-      print("Congragulations. You won the game!")
-      break
+def Hangman():
+    def play_game(word_list, num_lives=5):
+        num_lives=5
+        game = Hangman(word_list, num_lives)
+        while True:
+            if game.num_lives == 0:
+                print("You lost!")
+                break
+            if game.num_letters > 0:
+                game.ask_for_input()
+                elif game.num_lives != 0 or game.num_letters > 0:
+                    print("Congragulations. You won the game!")
+                    break
 
 play_game(["apple", "plum"])
